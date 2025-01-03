3578c19a9de694d4d57db81b4915bf9c1f21876e
Chirag Hasija <chiraghasija0609@gmail.com>
2025-01-03 16:51:19 +0530

    Add ALLOWED_HOSTS environment variableS

--- Backend/settings.py
+++ Backend/settings.py
@@ -140,14 +140,14 @@ LOGIN_REDIRECT_URL = 'api-home'
 LOGIN_URL = 'login'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST ='smtp.gmail.com'
+# EMAIL_HOST ='smtp.gmail.com'
 
-EMAIL_PORT = 587
+# EMAIL_PORT = 587
 
-EMAIL_USE_TLS = True
+# EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
