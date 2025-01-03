1dd70b6473af3b4a718b9dc05b5e82ae00075927
Chirag Hasija <chiraghasija0609@gmail.com>
2025-01-03 16:49:00 +0530

    Add ALLOWED_HOSTS environment variable

--- Backend/settings.py
+++ Backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = True
 DEBUG = os.environ.get('DEBUG','False').lower() == 'True'
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(' ')
 
 
 # Application definition
